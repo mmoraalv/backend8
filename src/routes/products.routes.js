f85bcd140658233d74335dbdc7287dacf045742e
mariomora <m.mora.alv@gmail.com>
2023-11-09 07:04:54 -0600

    Se implementa errors

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,27 +6,31 @@ import CustomError from '../services/errors/customError.js';
 import EErrors from '../services/errors/enums.js';
 import { generateProductErrorInfo } from "../services/errors/info.js";
 
-const productRouter = Router()
+const productRouter = Router();
 
-productRouter.get('/', getProducts)
-productRouter.get('/:id', getProduct)
+productRouter.get('/', getProducts);
+productRouter.get('/:id', getProduct);
+
 productRouter.post('/', (req, res, next) => {
     const { title, description, price, stock, code, category } = req.body;
     try {
         if (!title || !description || !price || !stock || !code || !category) {
-            CustomError.createError({
+            const error = CustomError.createError({
                 name: "Product creation error",
                 cause: generateProductErrorInfo({ title, description, price, stock, code, category }),
                 message: "One or more properties were incomplete or not valid.",
                 code: EErrors.INVALID_PRODUCT_ERROR
-            })
+            });
+            next(error);
+        } else {
+            next();
         }
-        next();
     } catch (error) {
         next(error);
     }
 }, passportError('jwt'), authorization(['admin']), postProduct);
-productRouter.put('/:id', passportError('jwt'), authorization('Admin'), putProduct)
-productRouter.delete('/:id', passportError('jwt'), authorization('Admin'), deleteProduct)
 
-export default productRouter
+productRouter.put('/:id', passportError('jwt'), authorization(['admin']), putProduct);
+productRouter.delete('/:id', passportError('jwt'), authorization(['admin']), deleteProduct);
+
+export default productRouter;
