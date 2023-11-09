f9d4cff429e7d018f9cd159f18c27f347f76014b
mariomora <m.mora.alv@gmail.com>
2023-11-09 05:35:43 -0600

    Se implementa errors

--- src/services/errors/info.js
+++ src/services/errors/info.js
@@ -7,3 +7,11 @@ export const generateUserErrorInfo = (user) => {
     * email: needs to be a String, received ${user.email}
     `
 }
+
+export const generateProductErrorInfo = (product) => {
+    return `One or more properties were incomplete or not valid.
+    List of required properties:
+    * title : needs to be a String, received ${product.title}
+    * price : needs to be a Number, received ${product.price}
+    * category : needs to be a String, received ${product.category}`
+}
